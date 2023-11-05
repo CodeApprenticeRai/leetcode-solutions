217df7d29111dcc913e17858c89e3204c0ee9299
Tare' Gaskin <tare.dgg@gmail.com>
2023-11-05 08:34:32 -0500

    possibly incomplete solutions added

---
+++ ValidSudoku.py
@@ -0,0 +1,24 @@
+'''
+Each row must contain the digits 1-9 without repetion.
+Each column must contain the digits 1-9 without repetion.
+Each f the nine 3X3 subboxes of the grid must contain
+the digits 1-9 without repetition.
+'''
+
+'''
+Build space
+'''
+
+# class Vector:
+#     def __init__(self, s1, s2, s3):
+#         self.dict = {"row": s1,
+#                      "column":s2,
+#                      "square":s3}
+#
+#
+# class Solution:
+#     def __init__(self):
+#         self.space =
+#         for i in range(9):
+#             for j in range(9):
+#                 for k in range(9):
